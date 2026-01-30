d94def27e8c578b3325c453285597f2ff684d4dc
alexlecornu <al1119@exeter.ac.uk>
2026-01-30 11:40:56 +0000

    DemoIncrement completed

--- DemoIncrement.java
+++ DemoIncrement.java
@@ -6,15 +6,14 @@ public class DemoIncrement {
         int b = a++ + 5;
         int x = 3;
         int y = ++x + 5;
+        int p = 3;
+        ++p;
+        int q = p + 5; 
 
-        System.out.print("a: " + a);
-        System.out.print(", b: " + b);
-        System.out.print(", x: " + x);
-        System.out.println(", y: " + y);
+        System.out.print("a: " + a + ", b: " + b + ", x: " + x + ", y: " + y + ", p: " + p + ", q: " + q + "\n" );
 
-        // Replace the xxxxx in the following with the correct term
-        System.out.println("i++ means xxxxx-increment");
-        System.out.println("++i means xxxxx-increment");
+        System.out.println("i++ means post-increment");
+        System.out.println("++i means pre-increment");
     }
 }
 
